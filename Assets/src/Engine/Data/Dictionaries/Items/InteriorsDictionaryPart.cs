d0b5f6ae2991437c820259a041944b420da92ac5
iisaev <iisaev@diasoft.ru>
2022-06-05 13:48:58 +0300

    items prefabs

--- Assets/src/Engine/Data/Dictionaries/Items/InteriorsDictionaryPart.cs
+++ Assets/src/Engine/Data/Dictionaries/Items/InteriorsDictionaryPart.cs
@@ -14,10 +14,23 @@ namespace Engine.Data
 		/// </summary>
         public static class Interiors
         {
-			public const long TV_1 = 100L;
-			public const long SHELF_1 = 500L;
-			public const long DOOR_BOX_1 = 510L;
-			public const long DOOR_1 = 511L;
+			public const long SINK_1 = 100000L;
+			public const long COUNTER_CORNER_1 = 100001L;
+			public const long COUNTER_EMPTY_1 = 100002L;
+			public const long COUNTER_SHELF_1 = 100003L;
+			public const long COUNTER_WASHING_1 = 100004L;
+			public const long FRIDGE_1 = 100005L;
+			public const long OVEN_1 = 100006L;
+			public const long KITCHEN_TABLE_1 = 100007L;
+			public const long KITCHEN_CHAIR_1 = 100008L;
+			public const long HALL_CHAIR_1 = 100100L;
+			public const long HALL_COUCH_1 = 100101L;
+			public const long HALL_TV_CABINET_1 = 100102L;
+			public const long TVSET_1 = 100103L;
+			public const long BATH_SINK_1 = 100200L;
+			public const long BATH_WASHER_1 = 100201L;
+			public const long BATH_1 = 100202L;
+			public const long DOOR_1 = 101000L;
 		}
 
 	}
---
+++ Assets/src/Engine/Logic/Locations/Objects/Editor/LocationDroppedItemBehaviourEditor.cs
@@ -0,0 +1,41 @@
+using Engine.Data.Factories;
+using Engine.Logic.Locations.Objects;
+using UnityEditor;
+using UnityEngine;
+
+namespace Engine.Logic.Locations.Editor
+{
+    
+    [CustomEditor(typeof(LocationDroppedItemBehaviour), true)]
+    public class LocationDroppedItemBehaviourEditor : CustomEditorT<LocationDroppedItemBehaviour>
+    {
+
+        public override void OnAdditionEditor()
+        {
+            var item = target.Target.Item;
+
+            GUILayout.Space(30f);
+
+            if (item == null)
+            {
+                APILayout.WarningBox("Item info by id " + target.Target.ItemInfo.ID + " not founded, try check resources.\nИнформация о предмете по ИД " + target.Target.ItemInfo.ID + " не найдена, проверьте ресурсы.");
+                
+                GUILayout.Space(30f);
+                if (GUILayout.Button("Reload item factory"))
+                    ItemFactory.Instance.ReloadFactory();
+                return;
+            }
+            
+            GUILayout.Label("name: " + item.Name + ", " + Localization.Instance.Get(item.Name));
+            GUILayout.Label("desctiption: " + item.Description  + ", " + Localization.Instance.Get(item.Description));
+            GUILayout.Label("weight: " + EntityCalculationService .GetWeightFormat(item.Weight));
+            Icon = item.Sprite?.texture;
+            
+            GUILayout.Space(30f);
+            if (GUILayout.Button("Reload item factory"))
+                ItemFactory.Instance.ReloadFactory();
+        }
+        
+    }
+    
+}
